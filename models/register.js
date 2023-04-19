504347b112488cf1b25d083719c88a02bdba61ac
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-19 19:38:26 +0530

    Register model

---
+++ models/register.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+	email: String,
+	pass: String,
+	role: String,
+	validationKey: String
+});
+
+const model = mongoose.model('Register', schema);
+module.exports = model;
