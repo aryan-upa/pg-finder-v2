4883a18c191522734ef1dfd0ca6808a5d64a3854
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-22 00:57:27 +0530

    setup passport for local-strategy authentication.

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 const {port} = require('./config.js');
 require('./utils/database_connect');
+const logins = require('./models/login');
 
 app.use(express.urlencoded({extended: true}));
+
+/* PASSPORT SETUP */
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+passport.use('passport-local', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'pass'
+}, logins.authenticate()));
+app.use(passport.initialize({}));
+passport.serializeUser(logins.serializeUser());
+passport.deserializeUser(logins.deserializeUser());
 
 app.get('/', (req, res) => {
     res.send("Jai shree ram!");
