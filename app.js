37097b97e33990f8f884677d44ce016f1d621e51
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-21 09:25:08 +0530

    added auth route handler.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const {port} = require('./config.js');
+require('./utils/database_connect');
+
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("Jai shree ram!");
 });
+
+/* router configurations */
+const authRouter = require('./routes/auth');
+
+app.use('/auth', authRouter);
 
 app.listen (port, () => {
     console.log("Listening to port : " + port);
