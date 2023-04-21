3c347891b7be483092a7f58dcc4f54b0cdb01dab
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-21 09:24:39 +0530

    created auth.js router and registration handler.

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const logins = require('../models/login');
+const registrations = require('../models/register');
+const {generateValidationKey} = require('../utils/key_generator');
+const {sendRegistrationEmail} = require('../utils/mail_sender');
+
+/* --------- REGISTRATION --------- */
+router.get('/registration', (req, res) => {
+	res.status(200).send('registration page');
+});
+
+router.post('/registration', async (req, res) => {
+	const {email, pass, role} = req.body;
+	const whether_login_present = await logins.findOne({email: email});
+
+	if (whether_login_present)
+		return res.status(400).send({error: 'user already exists'});
+
+	const whether_registration_present = await registrations.findOne({email: email});
+	if (whether_registration_present)
+		await registrations.deleteOne({email: email});
+
+	const validationKey = generateValidationKey();
+	await registrations.create({
+		email, pass, role, validationKey
+	});
+
+	const whether_email_sent = await sendRegistrationEmail(email, validationKey);
+	if (!whether_email_sent) {
+		console.log('email sending unsuccessful @ ' + Date.now());
+		return res.status(500).send({error: 'internal server error, please re-register!'});
+	}
+
+	console.log(`email sent @ ${new Date().toISOString()} to email : ${email}.`);
+	res.status(200).send({success: 'email sent successfully'});
+});
+
+module.exports = router;
