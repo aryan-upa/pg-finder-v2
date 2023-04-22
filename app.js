c1737bc67d614206b0bc9c48b9478a0f2d14e1cd
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-23 04:18:18 +0530

    configured some middlewares to make available userRoleID.

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const logins = require('./models/login');
 const session = require('express-session');
 const sessionStore = require('connect-mongodb-session')(session);
 const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const {addUserDet} = require("./middlewares/common");
 
 /* SERVER CONFIGURATIONS */
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 
 /* SESSION STORE */
@@ -46,11 +49,21 @@ app.use(passport.session({}));
 passport.serializeUser(logins.serializeUser());
 passport.deserializeUser(logins.deserializeUser());
 
+
+/* MIDDLEWARES */
+app.use(addUserDet);
+app.use((req, res, next) => {
+    res.locals.userRoleID = req.session.userRoleID || null;
+    next();
+});
+
+
 /* HOME ROUTE */
 app.get('/', (req, res) => {
     res.send({
         message: "Jai shree ram!",
-        user: req.user || null
+        user: req.user || null,
+        userRoleID: req.session.userRoleID || null
     });
 });
 
