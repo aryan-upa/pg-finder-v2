886c248e41ee0135ed857d38aa141af355d5e3d2
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-22 19:23:48 +0530

    modified registrationSchema to check name field.
    added loginSchema.

--- utils/validation_schemas.js
+++ utils/validation_schemas.js
@@ -7,8 +7,12 @@ const registrationSchema = joi.object({
 			'string.pattern.base': 'Please enter a valid email!'
 		}),
 	pass: joi.string().min(8).alphanum().required().messages({
+		'string.empty': 'Password is required!',
 		'any.only': 'Confirm password must be same as Password',
 		'string.min': 'Password must be 8 characters long.'
+	}),
+	name: joi.string().trim().required().messages({
+		'string.empty': 'Name can not be empty!',
 	}),
 	confirmPass: joi.ref('pass'),
 	role: joi.string().valid('rider', 'provider')
@@ -17,6 +21,18 @@ const registrationSchema = joi.object({
 		})
 }).options({abortEarly: false});
 
+const loginSchema = joi.object({
+	email: joi.string().email().pattern(new RegExp('^\\w+([\.-]?\\w+)*@\\w+([\.-]?\\w+)*(\\.\\w{2,3})+$')).required().messages({
+		'string.email': 'Please enter a valid email!',
+		'string.empty': 'Email is required!',
+		'string.pattern.base': 'Please enter a valid email!'
+	}),
+	pass: joi.string().alphanum().required().messages({
+		'string.empty': 'Password is required!',
+	})
+}).options({abortEarly: false});
+
 module.exports = {
 	registrationSchema,
+	loginSchema,
 }
