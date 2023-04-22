8d32975da7d42da284b8f9bd246e4e7e271b9328
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-22 14:03:08 +0530

    added session, session storing and cookie parsing methods. added some commenting.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
-const {port} = require('./config.js');
+const {port, databaseURL, sessionSecret} = require('./config.js');
 require('./utils/database_connect');
 const logins = require('./models/login');
+const session = require('express-session');
+const sessionStore = require('connect-mongodb-session')(session);
+const cookieParser = require('cookie-parser');
 
+/* SERVER CONFIGURATIONS */
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+
+/* SESSION STORE */
+const store = new sessionStore({
+    uri: databaseURL,
+    collection: 'sessionStore'
+});
+
+
+/* SESSION & COOKIE SETUP */
+app.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week,
+        secure: false
+    },
+    Store: store
+}));
+
 
 /* PASSPORT SETUP */
 const passport = require('passport');
@@ -15,18 +42,26 @@ passport.use('passport-local', new LocalStrategy({
     passwordField: 'pass'
 }, logins.authenticate()));
 app.use(passport.initialize({}));
+app.use(passport.session({}));
 passport.serializeUser(logins.serializeUser());
 passport.deserializeUser(logins.deserializeUser());
 
+/* HOME ROUTE */
 app.get('/', (req, res) => {
-    res.send("Jai shree ram!");
+    res.send({
+        message: "Jai shree ram!",
+        user: req.user || null
+    });
 });
 
-/* router configurations */
+
+/* ROUTER CONFIGURATION */
 const authRouter = require('./routes/auth');
 
 app.use('/auth', authRouter);
 
+
+/* LISTENING TO PORT */
 app.listen (port, () => {
     console.log("Listening to port : " + port);
 });
