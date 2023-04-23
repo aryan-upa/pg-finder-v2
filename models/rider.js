ed30537a5d0d3581a48fc07ac69a3cddc1c90cd1
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-23 22:35:58 +0530

    modified userSchema added post delete methods.

--- models/rider.js
+++ models/rider.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bookings = require('booking');
+const reviews = require('review');
+const providers = require('provider');
+const properties = require('property');
 
 const schema = new mongoose.Schema({
 	name: String,
@@ -25,6 +29,31 @@ const schema = new mongoose.Schema({
 		name: String,
 		relation: String,
 		phone: Number
+	}
+});
+
+schema.post('findOneAndDelete', async function (data) {
+	if (!(data.bookings.length === 0)) {
+		await bookings.deleteMany({$and: [{_id: {$in: data.bookings}}, {$completed: false}]});
+		await providers.updateMany(
+			{pendingBooking: {$in: data.bookings}},
+			{$pull: {pendingBooking: {$in: data.bookings}}}
+		);
+	}
+
+	if (!(data.reviews.length === 0)) {
+		await reviews.deleteMany({_id: {$in: data.reviews}});
+		await properties.updateMany(
+			{reviews: {$in: data.reviews}},
+			{$pull: {reviews: {$in: data.reviews}}}
+		);
+	}
+
+	if (!(data.likes.length === 0)) {
+		await properties.updateMany(
+			{_id: {$in: data.likes}},
+			{$inc: {interested: -1}}
+		);
 	}
 });
 
