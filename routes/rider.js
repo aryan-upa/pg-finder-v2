fb95f5c3c7e1e19c9b89fcc5779f4416b8a5caed
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-24 09:02:03 +0530

    created rider data input patch.

--- routes/rider.js
+++ routes/rider.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const riders = require('../models/rider');
 const {isRoleAdmin, isLoggedIn, isCurrentUserOrAdmin, isCurrentUser} = require("../middlewares/role_validator");
+const {validateRiderDetails} = require('../middlewares/schema_validator');
+const {uploadFile} = require("../utils/file_uploader");
 
 router.use(isLoggedIn);
 
@@ -21,10 +23,27 @@ router.get('/:id/edit', isCurrentUser, (req, res) => {
 	res.send('edit profile page for user ' + req.params.id);
 });
 
-router.post('/:id', isCurrentUser, (req, res) => {
+router.patch('/:id', isCurrentUser, validateRiderDetails, uploadFile('profile-pic'), uploadFile('covid-cert'), async (req, res) => {
+	const {id} = req.params;
+	const {phone, gender, dob, occupation, emContactName, emContactRelation, emContactPhone, imageLink, covidCertLink} = req.body;
+	const updateEmContact = {
+		name: emContactName,
+		relation: emContactRelation,
+		phone: emContactPhone
+	};
 
+	const newRider = await riders.findOneAndUpdate({id: id}, {
+		phone: phone,
+		gender: gender,
+		dob: dob,
+		occupation: occupation,
+		profilePic: imageLink,
+		covidCert: covidCertLink,
+		emergencyContact: updateEmContact
+	});
+
+	res.send({success: 'Profile Updated!'});
 });
-
 
 router.get('/:id/favourites', isCurrentUserOrAdmin, async (req, res) => {
 	const {id} = req.params;
