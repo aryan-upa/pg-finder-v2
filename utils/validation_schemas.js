b73b83bdef3de58cdd30353221067e95ffaba95f
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-24 08:59:34 +0530

    created riderSchema for validation.

--- utils/validation_schemas.js
+++ utils/validation_schemas.js
@@ -11,8 +11,9 @@ const registrationSchema = joi.object({
 		'any.only': 'Confirm password must be same as Password',
 		'string.min': 'Password must be 8 characters long.'
 	}),
-	name: joi.string().trim().required().messages({
+	name: joi.string().trim().pattern(new RegExp('^[A-Za-z]+$')).required().messages({
 		'string.empty': 'Name can not be empty!',
+		'string.pattern.base': 'Name contains invalid characters.',
 	}),
 	confirmPass: joi.ref('pass'),
 	role: joi.string().valid('rider', 'provider')
@@ -30,6 +31,35 @@ const loginSchema = joi.object({
 	pass: joi.string().alphanum().required().messages({
 		'string.empty': 'Password is required!',
 	})
+}).options({abortEarly: false});
+
+const riderSchema = joi.object({
+	name: joi.string().trim().pattern(new RegExp('^[A-Za-z]+$')).required().messages({
+		'string.pattern.base' : 'Name contains invalid characters.'
+	}),
+	phone: joi.string().pattern(new RegExp('^(?:(?:\\+|0{0,2})91(\\s*-\\s*)?|0?)?[6789]\d{9}$')).required().messages({
+		'string.empty': 'Please provide phone number.',
+		'string.pattern.base': 'Phone number invalid.'
+	}),
+	dob: joi.date().min(1990).max("now").required().messages({
+		'date.min' : 'Age less than minimum age.',
+		'date.max' : 'Age greater than maximum age.',
+		'date.empty' : 'DOB is required'
+	}),
+	gender: joi.string().trim().valid('male', 'female', 'trans').required().messages({
+		'any.only': 'invalid gender value.'
+	}),
+	occupation: joi.string().trim(),
+	emContactName: joi.string().trim().pattern(new RegExp('^[A-Za-z]+$')).required().messages({
+		'string.empty': 'require emergency contact name!',
+	}),
+	emContactRelation: joi.string().trim().required().messages({
+		'string.empty': 'require emergency contact relation!',
+	}),
+	emContactPhone: joi.string().pattern(new RegExp('^(?:(?:\\+|0{0,2})91(\\s*-\\s*)?|0?)?[6789]\d{9}$')).required().messages({
+		'string.empty': 'Please provide emergency contact phone number.',
+		'string.pattern.base': 'Phone number invalid.'
+	}),
 }).options({abortEarly: false});
 
 module.exports = {
