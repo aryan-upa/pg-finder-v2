82b833ac9c840e607e3e849413c411a75be7fadf
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-25 08:18:01 +0530

    added propertySchema

--- utils/validation_schemas.js
+++ utils/validation_schemas.js
@@ -127,9 +127,90 @@ const providerSchema = joi.object({
 	'zip.invalid': 'Zipcode does not match with state, please check again!',
 }).options({abortEarly: false});
 
+const propertySchema = joi.object({
+	name: joi.string().trim().pattern(new RegExp('^[A-Za-z]+$')).required().messages({
+		'string.pattern.base' : 'Name contains invalid characters.'
+	}),
+	addBuilding: joi.string().trim().required().min(5).max(50).messages({
+		'string.empty': 'Building Name can not be empty.',
+	}),
+	addL1: joi.string().trim().required().min(10).max(80).messages({
+		'string.empty': 'Address Line 1 is required.',
+	}),
+	landmark: joi.string().trim().required().min(10).max(80).messages({
+		'string.empty': 'Landmark is required',
+	}),
+	state: joi.string().trim().required().custom((state, helper) => {
+		if (!stateUTList.includes(state))
+			return helper.error("any.invalid");
+		return true;
+	}).messages({
+		'string.empty' : 'State can not be empty!',
+		'any.invalid' : 'Invalid state input!',
+	}),
+	city: joi.string().trim().required().messages({
+		'string.empty': 'City name can not be empty!',
+	}),
+	zipCode: joi.string().min(6).max(6).required().messages({
+		'string.min' : 'invalid zipcode',
+		'string.max' : 'invalid zipcode',
+		'string.empty' : 'zipcode required',
+	}),
+	maxOccupancy: joi.number().min(1).max(1000).required().messages({
+		'number.min': 'Minimum occupancy is 1',
+		'number max': 'Maximum occupancy is 1000',
+		'number.empty': 'Please provide occupancy'
+	}),
+	type: joi.string().required().valid('male', 'female','co').messages({
+		'string.empty': 'Please defined type.',
+		'any.only': 'Invalid type value'
+	}),
+	desc: joi.string().required().min(50).max(1000).messages({
+		'string.min': 'please write description at least 50 characters',
+		'string.max': 'please write description under 1000 characters',
+		'string.empty': 'description is required'
+	}),
+	occupancy: joi.array().items(joi.string().valid('single', 'double').messages({
+		'any.only': 'invalid occupancy type!',
+	})).min(1).messages({
+		'array.empty': 'occupancy can not be empty',
+		'array.min': 'choose at least 1 occupancy type!'
+	}),
+	rate: joi.number().min(1).required().messages({
+		'number.empty' : 'defining rate is required',
+		'number.min': 'rate has to be at least 1 rupee'
+	}),
+	since: joi.number().min(1947).max(new Date().getFullYear()).required().messages({
+		'number.min': 'year has to be at least 1947',
+		'number.max': 'year input is invalid',
+		'number.empty': 'required to defined year value!'
+	}),
+	tagLine: joi.string()
+}).custom ( async (obj, helper) => {
+	const {state, city, zipcode} = obj;
+
+	if (!cityMap[state].includes(city))
+		return helper.error('city.invalid');
+
+	const zipDetails = await getZipcodeDetails(zipcode);
+
+	if (zipDetails.error)
+		return helper.error('zip.verify');
+
+	if (zipDetails.state !== state)
+		return helper.error('zip.invalid');
+
+	return true;
+}).messages({
+	'city.invalid': 'City name is invalid for current state!',
+	'zip.verify' : 'Could not verify zipcode, please check zipcode and try again!',
+	'zip.invalid': 'Zipcode does not match with state, please check again!',
+}).options({abortEarly: false});
+
 module.exports = {
 	registrationSchema,
 	loginSchema,
 	riderSchema,
 	providerSchema,
+	propertySchema,
 }
