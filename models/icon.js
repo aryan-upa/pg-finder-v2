867488563a60def4be872b7e575d6fd52fe0b7dd
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-25 13:33:02 +0530

    removed icon file.

--- models/icon.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-	title: String,
-	url: String
-});
-
-const model = mongoose.model('Icon', schema);
-module.exports = model;
