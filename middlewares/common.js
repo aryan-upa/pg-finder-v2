2c76901e3354d1bf939d7cb8157020b3e28b073d
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-03 14:26:40 +0530

    bug fixing.

--- middlewares/common.js
+++ middlewares/common.js
@@ -4,13 +4,13 @@ const providers = require('../models/provider');
 async function addRoleID (req, res, next) {
 	if (req.user) {
 		if (req.session.userRoleID)
-			next();
+			return next();
 
 		const {username, role} = req.user;
 
-		if (role === 'rider') {
+		if (role === 'rider')
 			req.session.userRoleID = (await riders.findOne({email: username})).id;
-		}
+
 		else if (role === 'provider')
 			req.session.userRoleID = (await providers.findOne({email: username})).id;
 	}
