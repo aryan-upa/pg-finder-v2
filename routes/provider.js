0690ee1b9ddf6a71de7236b1701b550f3ca3e5e5
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-07 20:28:47 +0530

    added try-catch routing to the router.

--- routes/provider.js
+++ routes/provider.js
@@ -8,83 +8,132 @@ const logins = require('../models/login');
 router.use(isLoggedIn);
 
 router.get('/', isRoleAdmin, async (req, res) => {
-	const skip = req.query.skip || 0;
-	const providerList = await providers.find().skip(skip).limit(10);
-	res.send(providerList);
+	try {
+		const skip = req.query.skip || 0;
+		const providerList = await providers.find().skip(skip).limit(10);
+		res.send(providerList);
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.get('/:id', isCurrentUserOrAdmin, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findOne({_id: id});
-	await user.populate['properties', 'bookingCompleted', 'bookingPending'];
-	res.render('provider-dashboard', {user}); // working properly
+	try {
+		const {id} = req.params;
+		const user = await providers.findOne({_id: id});
+
+		if (!user)
+			return res.status(404).send({error: true, message: 'User not found!'});
+
+		await user.populate(['properties', 'bookingCompleted', 'bookingPending']);
+		res.render('provider-dashboard', {user}); // working properly
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.get('/:id/new-user', isCurrentUser, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findById({_id: id});
-	res.render('register-provider', {user}); // working properly
+	try {
+		const {id} = req.params;
+		const user = await providers.findById({_id: id});
+		res.render('register-provider', {user}); // working properly
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 })
 router.get('/:id/edit', isCurrentUser, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findById({_id: id});
-	res.render('update-provider', {user}); // working properly
+	try {
+		const {id} = req.params;
+		const user = await providers.findById({_id: id});
+		res.render('update-provider', {user}); // working properly
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.patch('/:id', isCurrentUser, validateProviderDetails, async (req, res) => {
-	const {id} = req.params;
-	const {email, phone, dob, gst, addBuilding, addL1, addL2, landmark, state, city, zipCode} = req.body;
+	try {
+		const {id} = req.params;
+		const {email, phone, dob, gst, addBuilding, addL1, addL2, landmark, state, city, zipCode} = req.body;
 
-	const address = {
-		building: addBuilding,
-		addL1: addL1,
-		addL2: addL2,
-		landmark: landmark,
-		city: city,
-		state: state,
-		zipcode: zipCode,
-		country: 'India'
+		const address = {
+			building: addBuilding,
+			addL1: addL1,
+			addL2: addL2,
+			landmark: landmark,
+			city: city,
+			state: state,
+			zipcode: zipCode,
+			country: 'India'
+		}
+
+		await providers.findOneAndUpdate({_id: id}, {
+			phone: phone,
+			dob: dob,
+			gst: gst,
+			address: address
+		});
+
+		await logins.findOneAndUpdate({username: email}, {isFilled: true});
+		res.send({success: 'Profile Updated!'}); // working properly
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
 	}
-
-	await providers.findOneAndUpdate({_id: id}, {
-		phone: phone,
-		dob: dob,
-		gst: gst,
-		address: address
-	});
-
-	await logins.findOneAndUpdate({username: email}, {isFilled: true});
-	res.send({success: 'Profile Updated!'}); // working properly
 });
 
 router.get('/:id/properties', isCurrentUserOrAdmin, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findOne({_id: id});
-	await user.populate('properties');
-	const likes = user.likes;
-	res.send(likes);
+	try {
+		const {id} = req.params;
+		const user = await providers.findOne({_id: id});
+		await user.populate('properties');
+		const likes = user.likes;
+		res.send(likes);
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.get('/:id/bookings-completed', isCurrentUserOrAdmin, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findOne({_id: id});
-	await user.populate('bookingCompleted');
-	const completedBookings = user.bookingCompleted;
-	res.send(completedBookings);
+	try {
+		const {id} = req.params;
+		const user = await providers.findOne({_id: id});
+		await user.populate('bookingCompleted');
+		const completedBookings = user.bookingCompleted;
+		res.send(completedBookings);
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.get('/:id/bookings-pending', isCurrentUserOrAdmin, async (req, res) => {
-	const {id} = req.params;
-	const user = await providers.findOne({_id: id});
-	await user.populate('bookingPending');
-	const pendingBookings = user.bookingPending;
-	res.send(pendingBookings);
+	try {
+		const {id} = req.params;
+		const user = await providers.findOne({_id: id});
+		await user.populate('bookingPending');
+		const pendingBookings = user.bookingPending;
+		res.send(pendingBookings);
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 router.delete('/:id', isRoleAdmin, async (req, res) => {
-	const {id} = req.params;
-	const info = await providers.deleteOne({_id: id});
-	res.send(info);
+	try {
+		const {id} = req.params;
+		const info = await providers.deleteOne({_id: id});
+		res.send({success: true, message: 'User deleted successfully!'});
+	} catch (e) {
+		console.log(e);
+		res.status(500).render('error', {error: 'Internal server error!'});
+	}
 });
 
 module.exports = router;
