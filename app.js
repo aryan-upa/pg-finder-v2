f54196d0b934791f9963c0e12812488eb6a1b54b
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-09 21:43:04 +0530

    setting up app.js for frontend.

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const session = require('express-session');
 const sessionStore = require('connect-mongodb-session')(session);
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
+const engine = require('ejs-mate');
+const path = require('path');
 const {addRoleID} = require("./middlewares/common");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://slick-peas-relax-223-233-65-180.loca.lt"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 /* SERVER CONFIGURATIONS */
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+app.set('view engine', "ejs");
+app.engine("ejs", engine);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 /* SESSION STORE */
@@ -60,6 +59,8 @@ passport.deserializeUser(logins.deserializeUser());
 app.use(addRoleID);
 app.use((req, res, next) => {
     res.locals.userRoleID = req.session.userRoleID || null;
+    res.locals.userDet = req.session.userDet || null;
+    res.locals.user = req.user;
     next();
 });
 
@@ -69,7 +70,8 @@ app.get('/', (req, res) => {
     res.send({
         message: "Jai shree ram!",
         user: req.user || null,
-        userRoleID: req.session.userRoleID || null
+        userRoleID: req.session.userRoleID || null,
+        userDet: req.session.userDet || null,
     });
 });
 
