db666d76ff802b285d2d0034c225b66fb09889d6
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-09 08:04:34 +0530

    review model updated to remove post middlewares.

--- models/review.js
+++ models/review.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const riders = require('./rider');
-const properties = require('./property');
 
 const schema = new mongoose.Schema({
 	rating: Number,
@@ -14,11 +12,6 @@ const schema = new mongoose.Schema({
 	},
 	comment: String,
 	date: Date
-});
-
-schema.post('findOneAndUpdate', async (data) => {
-	await properties.findByIdAndUpdate({id: data.propertyID}, {$pull: {reviews: {id: data.id}}});
-	await riders.findByIdAndUpdate({id: data.userID}, {$pull: {reviews: {id: data.id}}});
 });
 
 const model = mongoose.model('Review', schema);
