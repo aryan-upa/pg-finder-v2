f9030b07707bfa3000705dd23e9492890231548f
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-11 23:46:41 +0530

    added try-catch to all routes.

--- routes/booking.js
+++ routes/booking.js
@@ -11,69 +11,77 @@ const {sendBookingSuccessEmail, sendBookingFailEmail} = require('../utils/mail_s
 const {serverURL} = require('../config');
 
 router.get('/', isLoggedIn, isRoleAdmin, async (req, res) => {
-	let {skip} = req.query;
+	try {
+		let {skip} = req.query;
 
-	if (!skip || skip < 0) {
-		req.query.skip = 0;
-		skip = 0;
+		if (!skip || skip < 0) {
+			req.query.skip = 0;
+			skip = 0;
+		}
+
+		const results = await bookings.find({})
+			.populate({path: 'by'})
+			.populate({path: 'property'})
+			.skip(skip).limit(10);
+
+		return res.render('admin-details', {
+			type: 'booking',
+			results,
+			query: req.query,
+			isFirst: skip === 0,
+			isLast: results.length < 10,
+		});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
 	}
-
-	const results = await bookings.find({})
-		.populate({path: 'by'})
-		.populate({path: 'property'})
-		.skip(skip).limit(10);
-
-	return res.render('admin-details', {
-		type: 'booking',
-		results,
-		query: req.query,
-		isFirst: skip === 0,
-		isLast: results.length < 10,
-	});
-});
+})
 
 router.get('/payment-successful', isLoggedIn, isRoleRider, async (req, res) => {
-	const {propertyID, key} = req.query;
+	try {
+		const {propertyID, key} = req.query;
 
-	const keyGiven = await keys.findOne({key: key});
+		const keyGiven = await keys.findOne({key: key});
 
-	if (!keyGiven)
-		return res.render('error', {code: '404', error: 'invalid booking attempt'});
+		if (!keyGiven)
+			return res.render('error', {code: '404', error: 'invalid booking attempt'});
 
-	if (keyGiven.purpose !== 'payment')
-		return res.render('error', {code: '406', error: 'invalid booking attempt'});
+		if (keyGiven.purpose !== 'payment')
+			return res.render('error', {code: '406', error: 'invalid booking attempt'});
 
-	const property = await properties.findById({_id: propertyID});
+		const property = await properties.findById({_id: propertyID});
 
-	const config = {
-		url: `${serverURL}/booking?propertyID=${propertyID}&userID=${req.session.userRoleID}`,
-		method: 'post',
+		const config = {
+			url: `${serverURL}/booking?propertyID=${propertyID}&userID=${req.session.userRoleID}`,
+			method: 'post',
+		}
+
+		axios(config)
+			.then(async (response) => {
+				const bookingID = response.data.bookingID;
+				await sendBookingSuccessEmail(req.user.username, {
+					bookingID: bookingID,
+					customerName: req.session.userDet.name,
+					bookingDate: keyGiven.createdAt,
+					amountPaid: property.bookingMoney
+				});
+				res.render('success', {success: 'Your booking has been successful, Details have been mailed to you!'});
+			})
+			.catch(async (error) => {
+				await sendBookingFailEmail(req.user.username, {
+					amountPaid: property.bookingMoney,
+					customerName: req.session.userDet.name,
+					propertyID: propertyID,
+					paymentID: keyGiven.content.paymentID,
+					date: keyGiven.createdAt
+				});
+				res.render('error', {code: '500', error: 'Your booking was unsuccessful, details have been mailed to you!'});
+			});
+
+		await keys.findOneAndDelete({key: key});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
 	}
-
-	axios(config)
-		.then(async (response) => {
-			const bookingID = response.data.bookingID;
-			await sendBookingSuccessEmail(req.user.username, {
-				bookingID: bookingID,
-				customerName: req.session.userDet.name,
-				bookingDate: keyGiven.createdAt,
-				amountPaid: property.bookingMoney
-			});
-			res.render('success', {success: 'Your booking has been successful, Details have been mailed to you!'});
-		})
-		.catch(async (error) => {
-			await sendBookingFailEmail(req.user.username, {
-				amountPaid: property.bookingMoney,
-				customerName: req.session.userDet.name,
-				propertyID: propertyID,
-				paymentID: keyGiven.content.paymentID,
-				date: keyGiven.createdAt
-			});
-			res.render('error', {code: '500', error: 'Your booking was unsuccessful, details have been mailed to you!'});
-		});
-
-	await keys.findOneAndDelete({key: key});
-});
+})
 
 router.post('/', async (req, res) => {
 	try {
@@ -105,40 +113,38 @@ router.post('/', async (req, res) => {
 	} catch (e) {
 		res.status(500).send({error: 'internal server error!'});
 	}
-});
+})
 
 router.patch('/:id', isLoggedIn, isRoleProvider, async (req, res) => {
-	const {id: bookingID} = req.params;
-	const booking = await bookings.findById({_id: bookingID});
+	try {
+		const {id: bookingID} = req.params;
+		const booking = await bookings.findById({_id: bookingID});
 
-	const providerID = req.session.userRoleID;
-	if (booking.owner._id === providerID)
-		return res.status(406).send({error: 'Request denied!'});
+		const providerID = req.session.userRoleID;
+		if (booking.owner._id === providerID)
+			return res.status(406).send({error: 'Request denied!'});
 
-	if (booking.completed)
-		return res.status(403).send({error: 'Bad request, Booking already completed'});
+		if (booking.completed)
+			return res.status(403).send({error: 'Bad request, Booking already completed'});
 
-	const {res: result, comment} = req.body;
+		const {res: result, comment} = req.body;
 
-	booking.res = result === 'true';
-	booking.comment = comment;
-	booking.completed = true;
+		booking.res = result === 'true';
+		booking.comment = comment;
+		booking.completed = true;
 
-	await booking.save();
-	const updatedBooking = await bookings.findById({_id: bookingID});
-	await providers.findByIdAndUpdate(
-		{_id: providerID},
-		{$pull: {bookingPending: updatedBooking._id}, $push: {bookingCompleted: updatedBooking._id}}
-	);
+		await booking.save();
+		const updatedBooking = await bookings.findById({_id: bookingID});
+		await providers.findByIdAndUpdate(
+			{_id: providerID},
+			{$pull: {bookingPending: updatedBooking._id}, $push: {bookingCompleted: updatedBooking._id}}
+		);
 
-	res.send({success: 'booking updated'});
-});
-
-router.get('/:id', isLoggedIn, async (req, res) => {
-	const {id} = req.params;
-	const booking = await bookings.findById({id});
-	res.send(booking);
-});
+		res.send({success: 'booking updated'});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
+	}
+})
 
 router.delete('/:id', isLoggedIn, isRoleAdminOrProvider, async (req, res) => {
 	try {
