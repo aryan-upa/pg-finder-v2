a952482c4684213218d27a255c040c6ec68bd452
Aryan Upadhyay <aryan.offc@gmail.com>
2023-05-11 23:48:28 +0530

    added try-catch to all routes.

--- routes/review.js
+++ routes/review.js
@@ -6,63 +6,78 @@ const riders = require('../models/rider');
 const reviews = require('../models/review');
 
 router.get('/', isRoleAdmin, (req, res) => {
-	res.send({error: 'not defined'});
-});
+	try {
+		res.send({error: 'not defined'});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
+	}
+})
 
 router.get('/:id', async  (req, res) => {
-	const {id} = req.params;
-	const review = await reviews.findById({id});
-	await review.populate(['userID', 'propertyID']);
-	res.send({success: 'review found!', review});
-});
+	try {
+		const {id} = req.params;
+		const review = await reviews.findById({id});
+		await review.populate(['userID', 'propertyID']);
+		res.send({success: 'review found!', review});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
+	}
+})
 
 router.post('/', isLoggedIn, isRoleRider, async (req, res) => {
-	const {rating, comment} = req.body;
+	try {
+		const {rating, comment} = req.body;
 
-	const {propertyID} = req.query;
-	const property = await properties.findById({_id: propertyID});
+		const {propertyID} = req.query;
+		const property = await properties.findById({_id: propertyID});
 
-	const riderID = req.session.userRoleID;
-	const rider = await riders.findById({_id: riderID});
+		const riderID = req.session.userRoleID;
+		const rider = await riders.findById({_id: riderID});
 
-	const newReview = await reviews.create({
-		rating: rating,
-		userID: rider,
-		propertyID: property,
-		comment,
-		date: Date.now()
-	});
+		const newReview = await reviews.create({
+			rating: rating,
+			userID: rider,
+			propertyID: property,
+			comment,
+			date: Date.now()
+		});
 
-	rider.reviews.push(newReview);
-	rider.save();
+		rider.reviews.push(newReview);
+		rider.save();
 
-	const totalRating = property.reviews.length;
-	const newRating = ((property.rating * totalRating) + Number(rating)) / (totalRating + 1);
+		const totalRating = property.reviews.length;
+		const newRating = ((property.rating * totalRating) + Number(rating)) / (totalRating + 1);
 
-	property.reviews.push(newReview);
-	property.rating = newRating;
+		property.reviews.push(newReview);
+		property.rating = newRating;
 
-	property.save();
-	return res.send({success: 'review saved successfully'});
-});
+		property.save();
+		return res.send({success: 'review saved successfully'});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
+	}
+})
 
 router.delete('/:id', isLoggedIn, isRoleAdminOrRider, async (req, res) => {
-	const {id} = req.params;
-	const review = await reviews.findById({_id: id});
+	try {
+		const {id} = req.params;
+		const review = await reviews.findById({_id: id});
 
-	if (!review)
-		return res.status(404).send({error: 'review not found!'});
+		if (!review)
+			return res.status(404).send({error: 'review not found!'});
 
-	if (!review.userID.equals(req.session.userRoleID))
-		return res.status(406).send({error: 'not authorized!'});
+		if (!review.userID.equals(req.session.userRoleID))
+			return res.status(406).send({error: 'not authorized!'});
 
-	const info = await reviews.findByIdAndDelete({_id: id});
+		const info = await reviews.findByIdAndDelete({_id: id});
 
-	await properties.findByIdAndUpdate(review.propertyID, {$pull: {reviews: id}});
-	await riders.findByIdAndUpdate(review.userID, {$pull: {reviews: id}})
+		await properties.findByIdAndUpdate(review.propertyID, {$pull: {reviews: id}});
+		await riders.findByIdAndUpdate(review.userID, {$pull: {reviews: id}})
 
-	res.send({success: 'review deleted successfully', info});
-});
-
+		res.send({success: 'review deleted successfully', info});
+	} catch (e) {
+		res.render('error', {code: 500, error: 'Internal server error'})
+	}
+})
 
 module.exports = router;
